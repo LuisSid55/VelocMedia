9d0a11bef42bd758540f3dcd3d2071bcce2e73e9
LuisSid55 <luiscitty4@gmail.com>
2021-09-26 18:33:16 -0300

    VelocMedia

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double v, d, t;
+
+            Console.WriteLine("\n--Velocidade média--\n");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Para calcular a velocidade média, digite a ");
+            Console.WriteLine("distância (metros) e tempo(segundos).\n");
+            
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Write("Distancia percorrida (m): ");
+            d = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Tempo gasto (s): ");
+            t = Convert.ToDouble(Console.ReadLine());
+
+            v = d / t;
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"\nA velocidade média é: {v:N2} m/s\n");
+            Console.ResetColor();
+        }
+    }
+}
